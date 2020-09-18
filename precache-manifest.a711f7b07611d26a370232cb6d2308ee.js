c80ef13eeb43090715d8ef7cc48fe60279cb1808
addygenerale <sammcaddy@gmail.com>
2020-09-18 06:52:24 +0000

    Updates

---
+++ precache-manifest.a711f7b07611d26a370232cb6d2308ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39cc53764269e779652e312ad2b4ab4",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "6f4042c00eeca9ac5bc8",
+    "url": "/currency-converter/static/css/main.f17919a7.chunk.css"
+  },
+  {
+    "revision": "8d3e09f8bc2793a439e5",
+    "url": "/currency-converter/static/js/2.b8196ae1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter/static/js/2.b8196ae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f4042c00eeca9ac5bc8",
+    "url": "/currency-converter/static/js/main.3818f976.chunk.js"
+  },
+  {
+    "revision": "5e05a0ce7437735b2d17",
+    "url": "/currency-converter/static/js/runtime-main.e7c55d23.js"
+  }
+]);
